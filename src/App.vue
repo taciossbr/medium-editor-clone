59069c28d0603f706258c83e71b4a65ddd81bfb7
taciossbr <tacioss.br@gmail.com>
2018-09-10 15:47:53 -0300

    Start the medium editor clone

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <editor v-bind:content="text" v-on:update="text = $event"/>
+  </div>
+</template>
+
+<script>
+import Editor from './components/Editor';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      text: '',
+    }
+  },
+  components: {
+    'editor': Editor,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.3em;
+}
+</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="editor-wrap">
+    <div 
+      class="editor" 
+      contenteditable="true" 
+      v-on:input="change"
+      ref="editor"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Editor',
+  props: ['content'],
+  mounted() {
+    document.execCommand("defaultParagraphSeparator", false, "p");
+    this.$refs.editor.innerHTML = this.content;
+  },
+  methods: {
+    change() {
+      
+      this.$emit('update', this.$refs.editor.innerHTML)
+    },
+}
+</script>
+
+<style scoped>
+  .editor:empty::after {
+    content: 'Escreva Aqui';
+    color: #cecece;
+    font-style: italic;
+    margin-left: 10px;
+  }
+  .editor:focus {
+    border: none;
+    outline: none;
+  }
+</style>
