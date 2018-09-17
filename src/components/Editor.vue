2c9249487a910f344ba73cfd95540f8e6c4404a2
taciossbr <tacioss.br@gmail.com>
2018-09-17 17:45:00 -0300

    implementação da interface básica

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,7 +4,9 @@
       class="editor" 
       contenteditable="true" 
       v-on:input="change"
-      ref="editor"></div>
+      ref="editor"
+      ></div>
+      <p v-on:click="newParagraph" class='new-paragraph'>Escreva Aqui</p>
   </div>
 </template>
 
@@ -14,23 +16,45 @@ export default {
   props: ['content'],
   mounted() {
     document.execCommand("defaultParagraphSeparator", false, "p");
-    this.$refs.editor.innerHTML = this.content;
+    document.execCommand("insertBrOnReturn", false, false);
+    this.$refs.editor.innerHTML = this.content || '<h1></h1>';
   },
   methods: {
     change() {
-      
-      this.$emit('update', this.$refs.editor.innerHTML)
+      if(!this.$refs.editor.innerHTML) {
+        this.$refs.editor.innerHTML = '<h1></h1>'
+        // this.isEmpty = true;
+      } else {
+        // this.isEmpty = false;
+        this.$emit('update', this.$refs.editor.innerHTML);
+      }
     },
+    newParagraph() {
+      // this.$refs.editor.focus();
+      // console.log(this.$refs.editor);
+      let char = this.$refs.editor.childNodes.length;
+      // console.log(char)
+      let sel = window.getSelection();
+      sel.collapse(this.$refs.editor, char);
+      document.execCommand("insertParagraph", false);
+
+      
+    }
   }
 }
 </script>
 
-<style scoped>
-  .editor:empty::after {
-    content: 'Escreva Aqui';
+<style>
+  .editor > h1:empty::after, .new-paragraph { 
     color: #cecece;
     font-style: italic;
     margin-left: 10px;
+  } 
+  .editor > h1:empty::after {
+    content: 'Titulo';
+  }
+  .new-paragraph {
+    content: 'Escreva Aqui';
   }
   .editor:focus {
     border: none;
